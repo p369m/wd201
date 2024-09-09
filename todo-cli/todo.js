a39cc6ee612e696788e4fae6d0534c8ee05bff3e
PARTHA MANDAL <125090780+p369m@users.noreply.github.com>
2024-09-09 15:13:14 +0530

    L3 fix

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -10,7 +10,7 @@ const todoList = () => {
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
-    return all.filter((todo) => !todo.completed && todo.dueDate === yesterday);
+    return all.filter((todo) => !todo.completed && todo.dueDate < today);
   };
 
   const dueToday = () => {
@@ -22,7 +22,7 @@ const todoList = () => {
   const dueLater = () => {
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
-    return all.filter((todo) => !todo.completed && todo.dueDate === tomorrow);
+    return all.filter((todo) => !todo.completed && todo.dueDate > today);
   };
 
   const toDisplayableList = (list) => {
